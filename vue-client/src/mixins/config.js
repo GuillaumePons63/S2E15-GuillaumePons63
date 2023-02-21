8aee6bc95f611462dea1ff46890218a470f810e1
Kenshirosan <laurent.sama@gmail.com>
2023-02-21 15:21:56 +0100

    run with proxy by default

--- vue-client/src/mixins/config.js
+++ vue-client/src/mixins/config.js
@@ -5,8 +5,8 @@ export default {
             // dev_server_url: 'http://localhost:5000/api',
             // docker_dev_url: 'http://redis-server:5000',
 
-            server_url: 'http://localhost:5000/api',
-            dev_server_url: 'http://localhost:5000/api',
+            server_url: 'http://localhost/api',
+            dev_server_url: 'http://localhost/api',
             docker_dev_url: 'http://redis-server:5000',
         };
     },
